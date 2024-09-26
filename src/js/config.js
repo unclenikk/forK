9ddca70b004b20cd6467f814085a3ce7d424817b
unclenikk <nikaamrazishvili@gmail.com>
2024-09-26 23:47:59 +0400

    finish

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8112540b-12eb-4d28-86ee-286c8dcf781a';
+export const MODAL_CLOSE_SEC = 2.5;
+
+
